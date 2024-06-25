35cbc4a451a4d2845d6c71fd0e10caa79e8304a5
WonTae Kim <rladnjsxo123@naver.com>
2024-06-25 19:51:43 +0900

    refactor: name changing and fibonacci lib

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,8 +1,12 @@
 import test from 'ava'
 
-import { plus100 } from '../index'
+import { plus100, fibonacci } from '../index'
 
 test('sync function from native code', (t) => {
   const fixture = 42
   t.is(plus100(fixture), fixture + 100)
+})
+
+test('fibonacci', (t) => {
+  t.is(fibonacci(3), 2)
 })
--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,11 @@ use napi_derive::napi;
 pub fn plus_100(input: u32) -> u32 {
   input + 100
 }
+
+#[napi]
+pub fn fibonacci(n: u32) -> u32 {
+  match n {
+    1 | 2 => 1,
+    _ => fibonacci(n - 1) + fibonacci(n - 2),
+  }
+}
